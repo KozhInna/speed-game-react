84fc0814ed4497c28f5570213a5bc0331a3f2e77
Inna Kozhina <inna.kozhina22@gmail.com>
2024-01-14 20:32:16 +0200

    deleted unused file

--- src/UI_components/Circle.jsx
+++ src/UI_components/Circle.jsx
@@ -1,7 +1,6 @@
 function Circle({ circleClickHandler, current, id }) {
   return (
     <div
-      /* className={styles.circle} */
       className={`circle ${current ? "active" : ""}`}
       onClick={() => circleClickHandler(id)}
     ></div>
